b056d81bc88dabb2ed066a43caa08130d7ad76ae
cvkulkarnidev <cv.kulkarni.dev@gmail.com>
2026-06-11 16:34:32 +0530

    Add EAGLE-3 draft package init

---
+++ eagle3_draft_training/src/eagle3_draft/__init__.py
@@ -0,0 +1,6 @@
+"""Lightweight EAGLE-3-style drafter training package."""
+
+from .config import Eagle3TrainingConfig
+from .model import Eagle3DraftModel
+
+__all__ = ["Eagle3TrainingConfig", "Eagle3DraftModel"]
